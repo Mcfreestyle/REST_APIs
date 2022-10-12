da3421db7bb1b55da8b699c04cd80fc6d835f0fd
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-11 22:24:07 -0500

    feat: function that create sql script

---
+++ flask-aiomysql/utils/create_sql_script.py
@@ -0,0 +1,40 @@
+def create_SQL_script(tables_dict={}, database_name=''):
+  """
+    Create a dummy file
+    Args:
+      tables_dict = For example:
+        {
+          users: ["(1, 'Michael', '123', ...)", "(2, 'Destino', '456', ...)"],
+          todo_lists: ["(1, 'lista1', ...)", "(2, 'lista2', ...)"]
+        }
+  """
+  filename = "./sql-scripts/feed_database.sql"
+
+  idx = 0
+  spacing = "\n\n\n"
+
+  start = """--
+-- Dumping dummy data for {0}
+--
+LOCK TABLES `{0}` WRITE;
+INSERT INTO `{0}` VALUES
+"""
+
+  end = "UNLOCK TABLES;"
+
+  with open(filename, mode="w", encoding="utf-8") as new_file:
+    idx += new_file.write(spacing)
+    idx += new_file.write("USE {};".format(database_name))
+
+    for key, value in tables_dict.items():
+      idx += new_file.write(spacing)
+      idx += new_file.write(start.format(key))
+      # loop over list (value)
+      for row in value:
+        row = (row + ';') if row == value[-1] else (row + ',')
+        idx += new_file.write(row)
+        idx += new_file.write("\n")
+
+      idx += new_file.write(end)
+
+  print("{} bytes written in file {}".format(idx, filename))
