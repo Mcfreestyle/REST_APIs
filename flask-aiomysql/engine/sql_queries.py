41f2c8238781a05152d48315123d4d6376ddde8e
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-27 13:08:52 -0500

    style: use of pycodestyle

--- flask-aiomysql/engine/sql_queries.py
+++ flask-aiomysql/engine/sql_queries.py
@@ -1,60 +1,70 @@
+"""Queries to export and use in the engine
 """
-  Queries to export and use
-  in the engine
-"""
+
 
 class MySQLQuery:
-  @staticmethod
-  def select_all(table):
-    """
-      Query to select all rows
-      from given table
-    """
-    return 'SELECT * FROM {}'.format(table)
+    @staticmethod
+    def select_all(table):
+        """Query to select all rows from given table
 
-  @staticmethod
-  def select_by_id(table):
-    """
-      Query from table using id
-    """
-    return 'SELECT * FROM {} WHERE id=%s'.format(table)
-  
-  @staticmethod
-  def insert(table, data = {}):
-    """
-      Insert new row into table
-    """
-    query = 'INSERT INTO {} '.format(table)
-    
-    keys = data.keys()
-    columns = ', '.join(keys)
-    
-    query = '{}({})'.format(query, columns)
+        Args:
+            table (str): Table name
+        """
+        return 'SELECT * FROM {}'.format(table)
 
-    place_holders = []
-    for k in keys:
-      place_holders.append('%({})s'.format(k))
-    
-    query = '{}VALUES ({})'.format(query, ', '.join(place_holders))
-    return query
+    @staticmethod
+    def select_by_id(table):
+        """Query from table using id
 
-  @staticmethod
-  def update(table, data={}):
-    """
-      Updates a row
-    """
-    query = 'UPDATE {} SET'.format(table)
+        Args:
+            table (str): Table name
+        """
+        return 'SELECT * FROM {} WHERE id=%s'.format(table)
 
-    set_list = []
-    for key, value in data.items():
-      set_list.append("{}='{}'".format(key, value))
+    @staticmethod
+    def insert(table, data={}):
+        """Insert new row into table
 
-    query = '{} {} WHERE id=%s'.format(query, ', '.join(set_list))
-    return query
+        Args:
+            table (str): Table name
+            data (dict): Record data
+        """
+        query = 'INSERT INTO {} '.format(table)
 
-  @staticmethod
-  def delete(table):
-    """
-      Delete a row
-    """
-    return 'DELETE FROM {} WHERE id=%s'.format(table)
+        keys = data.keys()
+        columns = ', '.join(keys)
+
+        query = '{}({})'.format(query, columns)
+
+        place_holders = []
+        for k in keys:
+            place_holders.append('%({})s'.format(k))
+
+        query = '{}VALUES ({})'.format(query, ', '.join(place_holders))
+        return query
+
+    @staticmethod
+    def update(table, data={}):
+        """Updates a row
+
+        Args:
+            table (str): Table name
+            data (dict): Record data
+        """
+        query = 'UPDATE {} SET'.format(table)
+
+        set_list = []
+        for key, value in data.items():
+            set_list.append("{}='{}'".format(key, value))
+
+        query = '{} {} WHERE id=%s'.format(query, ', '.join(set_list))
+        return query
+
+    @staticmethod
+    def delete(table):
+        """Delete a row
+
+        Args:
+            table (str): Table name
+        """
+        return 'DELETE FROM {} WHERE id=%s'.format(table)
