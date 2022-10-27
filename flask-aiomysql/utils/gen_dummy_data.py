ad12232c8cd608cdf03fd9cc51f7f44b6d71c82d
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-27 13:10:16 -0500

    style: use of pycodestyle

--- flask-aiomysql/utils/gen_dummy_data.py
+++ flask-aiomysql/utils/gen_dummy_data.py
@@ -1,31 +1,26 @@
-"""
-  GENERATES DUMMY DATA
+"""Generates dummy data
 """
 
 import random
-import datetime
 import lorem
 from faker import Faker
 from create_sql_script import create_SQL_script
 
 
 def random_date(start_date='-10d', end_date='+1M'):
-  fake = Faker()
-  return fake.date_time_between(start_date=start_date, end_date=end_date)
+    fake = Faker()
+    return fake.date_time_between(start_date=start_date, end_date=end_date)
+
 
 users = [
-  'Michael',
-  'Destino',
-  'Mcfree'
+    'Michael',
+    'Destino',
+    'Mcfree'
 ]
 
 nUsers = len(users)
 nTodoLists = 10
 nTasks = 50
-
-"""
-  generate data for MySQL
-"""
 
 """
 ==============
@@ -34,15 +29,15 @@ users
 """
 users_list = []
 for i in range(nUsers):
-  user_id = i + 1
-  user_name = users[i]
-  password = 'pwd{}'.format(i + 1)
-  created_at = random_date()
-  updated_at = random_date(created_at)
+    user_id = i + 1
+    user_name = users[i]
+    password = 'pwd{}'.format(i + 1)
+    created_at = random_date()
+    updated_at = random_date(created_at)
 
-  users_list.append("""('{}', '{}', '{}', '{}', '{}')""".format(
-    user_id, user_name, password, created_at, updated_at
-  ))
+    users_list.append("""('{}', '{}', '{}', '{}', '{}')""".format(
+        user_id, user_name, password, created_at, updated_at
+    ))
 
 """
 ==============
@@ -52,25 +47,25 @@ todo_lists
 todo_lists = []
 # Create a default list for each user
 for i in range(nUsers):
-  todo_list_id = i + 1
-  name = 'Default'
-  user_id = i + 1
-  created_at = random_date()
-  updated_at = random_date(created_at)
+    todo_list_id = i + 1
+    name = 'Default'
+    user_id = i + 1
+    created_at = random_date()
+    updated_at = random_date(created_at)
 
-  todo_lists.append("""('{}', '{}', '{}', '{}', '{}')""".format(
-    todo_list_id, name, user_id, created_at, updated_at
-  ))
+    todo_lists.append("""('{}', '{}', '{}', '{}', '{}')""".format(
+        todo_list_id, name, user_id, created_at, updated_at
+    ))
 for i in range(nUsers, nTodoLists):
-  todo_list_id = i + 1
-  name = 'lista{}'.format(i + 1)
-  user_id = random.randrange(nUsers) + 1
-  created_at = random_date()
-  updated_at = random_date(created_at)
+    todo_list_id = i + 1
+    name = 'lista{}'.format(i + 1)
+    user_id = random.randrange(nUsers) + 1
+    created_at = random_date()
+    updated_at = random_date(created_at)
 
-  todo_lists.append("""('{}', '{}', '{}', '{}', '{}')""".format(
-    todo_list_id, name, user_id, created_at, updated_at
-  ))
+    todo_lists.append("""('{}', '{}', '{}', '{}', '{}')""".format(
+        todo_list_id, name, user_id, created_at, updated_at
+    ))
 
 """
 ==============
@@ -79,17 +74,17 @@ tasks
 """
 tasks_list = []
 for i in range(nTasks):
-  task_id = i + 1
-  name = str(lorem.sentence())
-  created_at = random_date()
-  updated_at = random_date(created_at)
-  due_date = updated_at
-  todo_list_id = random.randrange(nTodoLists) + 1
-  status = random.randrange(2)
+    task_id = i + 1
+    name = str(lorem.sentence())
+    created_at = random_date()
+    updated_at = random_date(created_at)
+    due_date = updated_at
+    todo_list_id = random.randrange(nTodoLists) + 1
+    status = random.randrange(2)
 
-  tasks_list.append("""('{}', '{}', '{}', '{}', '{}', '{}', '{}')""".format(
-    task_id, name, due_date, todo_list_id, status, created_at, updated_at
-  ))
+    tasks_list.append("""('{}', '{}', '{}', '{}', '{}', '{}', '{}')""".format(
+        task_id, name, due_date, todo_list_id, status, created_at, updated_at
+    ))
 
 """
 =======================================
