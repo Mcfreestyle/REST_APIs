52f3826fcfcacc5c3f3b1dbace866e187fee7849
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-27 13:09:28 -0500

    style: use of pycodestyle

--- flask-aiomysql/utils/create_sql_script.py
+++ flask-aiomysql/utils/create_sql_script.py
@@ -1,40 +1,44 @@
+"""This module supplies the `create_SQL_script` function
+"""
+
+
 def create_SQL_script(tables_dict={}, database_name=''):
-  """
-    Create a dummy file
+    """Create a dummy file
+
     Args:
-      tables_dict = For example:
-        {
-          users: ["(1, 'Michael', '123', ...)", "(2, 'Destino', '456', ...)"],
-          todo_lists: ["(1, 'lista1', ...)", "(2, 'lista2', ...)"]
-        }
-  """
-  filename = "./sql-scripts/feed_database.sql"
+        tables_dict (dict) = Database data, for example:
+            {
+                users: ["(1, 'Michael', ...)", "(2, 'Destino', ...)"],
+                todo_lists: ["(1, 'lista1', ...)", "(2, 'lista2', ...)"]
+            }
+    """
+    filename = "./sql-scripts/feed_database.sql"
 
-  idx = 0
-  spacing = "\n\n\n"
+    idx = 0
+    spacing = "\n\n\n"
 
-  start = """--
+    start = """--
 -- Dumping dummy data for {0}
 --
 LOCK TABLES `{0}` WRITE;
 INSERT INTO `{0}` VALUES
 """
 
-  end = "UNLOCK TABLES;"
+    end = "UNLOCK TABLES;"
 
-  with open(filename, mode="w", encoding="utf-8") as new_file:
-    idx += new_file.write(spacing)
-    idx += new_file.write("USE {};".format(database_name))
+    with open(filename, mode="w", encoding="utf-8") as new_file:
+        idx += new_file.write(spacing)
+        idx += new_file.write("USE {};".format(database_name))
 
-    for key, value in tables_dict.items():
-      idx += new_file.write(spacing)
-      idx += new_file.write(start.format(key))
-      # loop over list (value)
-      for row in value:
-        row = (row + ';') if row == value[-1] else (row + ',')
-        idx += new_file.write(row)
-        idx += new_file.write("\n")
+        for key, value in tables_dict.items():
+            idx += new_file.write(spacing)
+            idx += new_file.write(start.format(key))
+            # loop over list (value)
+            for row in value:
+                row = (row + ';') if row == value[-1] else (row + ',')
+                idx += new_file.write(row)
+                idx += new_file.write("\n")
 
-      idx += new_file.write(end)
+            idx += new_file.write(end)
 
-  print("{} bytes written in file {}".format(idx, filename))
+    print("{} bytes written in file {}".format(idx, filename))
