2c05562fd37071bb04e39b2814461c24921dbe50
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-27 13:05:57 -0500

    docs: comments of  function

--- flask-aiomysql/api/v1/views/users.py
+++ flask-aiomysql/api/v1/views/users.py
@@ -1,16 +1,25 @@
+"""This module supplies the `get_users` view function
+"""
+
 # import asyncio
 import threading
 from api.v1.views import app_views
 from flask import make_response, jsonify
 from engine.db_storage import DBStorage
 
+
 @app_views.route('/users', methods=['GET'])
 async def get_users():
-  print(f"Inside flask function: {threading.current_thread().name}")
-  # asyncio.set_event_loop(asyncio.new_event_loop())
-  # loop = asyncio.get_event_loop()
-  # users = loop.run_until_complete(storage.all('users', loop))
+    """Get all users
 
-  storage = DBStorage()
-  users = await storage.all('users')
-  return make_response(jsonify(users), 200)
+    Returns:
+        dict: All users of database
+    """
+    print(f"Inside flask function: {threading.current_thread().name}")
+    # asyncio.set_event_loop(asyncio.new_event_loop())
+    # loop = asyncio.get_event_loop()
+    # users = loop.run_until_complete(storage.all('users', loop))
+
+    storage = DBStorage()
+    users = await storage.all('users')
+    return make_response(jsonify(users), 200)
